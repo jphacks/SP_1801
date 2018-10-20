ca1afa5d69a9d88baa804515137fb2f0ff4397dd
RyokaNAKAI <b1014139@gmail.com>
2018-10-20 10:00:47 +0900

    Initial Commit

---
+++ TyokinshiAI_JPHACKS2018Tests/TyokinshiAI_JPHACKS2018Tests.swift
@@ -0,0 +1,36 @@
+//
+//  TyokinshiAI_JPHACKS2018Tests.swift
+//  TyokinshiAI_JPHACKS2018Tests
+//
+//  Created by 中井崚日 on 2018/10/20.
+//  Copyright © 2018年 中井崚日. All rights reserved.
+//
+
+import XCTest
+@testable import TyokinshiAI_JPHACKS2018
+
+class TyokinshiAI_JPHACKS2018Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
