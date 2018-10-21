8461df14acc819ab54b1a5958cddb35e35cd1b25
RyokaNAKAI <b1014139@gmail.com>
2018-10-21 10:48:22 +0900

    JPHACKS2018　モック完成ver1

---
+++ TyokinshiAI_JPHACKS2018/ViewController4.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController4.swift
+//  TyokinshiAI_JPHACKS2018
+//
+//  Created by 中井崚日 on 2018/10/20.
+//  Copyright © 2018年 中井崚日. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewController4: UIViewController {
+    
+}
---
+++ TyokinshiAI_JPHACKS2018/ViewController5.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController5.swift
+//  TyokinshiAI_JPHACKS2018
+//
+//  Created by 中井崚日 on 2018/10/20.
+//  Copyright © 2018年 中井崚日. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewController5: UIViewController {
+    
+}
